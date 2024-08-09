7b0e99640b1af73969186eee27f93e009d487a7a
Pascal Nehlsen <pascaldev92@gmail.com>
2024-08-09 14:13:06 +0200

    added user table design

--- src/app/dialog-add-user/dialog-add-user.component.ts
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { MatDialogModule } from '@angular/material/dialog';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
@@ -9,6 +9,7 @@ import { provideNativeDateAdapter } from '@angular/material/core';
 import { User } from '../../models/user.class';
 import { FormsModule } from '@angular/forms';
 import { addDoc, collection, Firestore } from '@angular/fire/firestore';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-dialog-add-user',
@@ -22,27 +23,33 @@ import { addDoc, collection, Firestore } from '@angular/fire/firestore';
     MatFormFieldModule,
     MatDatepickerModule,
     FormsModule,
+    MatProgressBarModule,
   ],
   templateUrl: './dialog-add-user.component.html',
   styleUrl: './dialog-add-user.component.scss',
 })
 export class DialogAddUserComponent {
+  constructor(public dialogRef: MatDialogRef<DialogAddUserComponent>) {}
+
   private firestore: Firestore = inject(Firestore);
   user: User = new User();
+
+  loading = false;
 
   birthDate!: Date;
 
   saveUser() {
+    this.loading = true;
     if (this.birthDate) {
       this.user.birthDate = this.birthDate.getTime();
     }
-    console.log('Current user is', this.user);
-
     const colRef = collection(this.firestore, 'users');
 
     addDoc(colRef, this.user.toJSON())
       .then((result: any) => {
+        this.loading = false;
         console.log('Adding user finished', result);
+        this.dialogRef.close();
       })
       .catch((error) => {
         console.error('Error adding user: ', error);
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -5,11 +5,12 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from '../../models/user.class';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [MatIconModule, MatTooltipModule, MatButtonModule],
+  imports: [MatIconModule, MatTooltipModule, MatButtonModule, MatCardModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
 })
