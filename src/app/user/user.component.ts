2e457c88ac1f747e009c0e7808980c2014f244b1
Pascal Nehlsen <pascaldev92@gmail.com>
2024-08-11 22:53:24 +0200

    rendering users from firebase

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { MatButtonModule } from '@angular/material/button';
@@ -6,6 +6,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { User } from '../../models/user.class';
 import { MatCardModule } from '@angular/material/card';
+import { collection, Firestore, onSnapshot } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-user',
@@ -14,9 +15,35 @@ import { MatCardModule } from '@angular/material/card';
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss',
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
   constructor(public dialog: MatDialog) {}
 
+  allUsers: any[] = [];
+
+  private firestore: Firestore = inject(Firestore);
+
+  ngOnInit(): void {
+    const colRef = collection(this.firestore, 'users');
+
+    const unsub = onSnapshot(colRef, (snapshot) => {
+      this.allUsers = [];
+      snapshot.forEach((doc) => {
+        this.allUsers.push(doc.data());
+      });
+
+      snapshot.docChanges().forEach((change) => {
+        if (change.type === 'added') {
+          console.log('Neues Dokument:', change.doc.data());
+        }
+        if (change.type === 'modified') {
+          console.log('Geändertes Dokument:', change.doc.data());
+        }
+        if (change.type === 'removed') {
+          console.log('Gelöschtes Dokument:', change.doc.data());
+        }
+      });
+    });
+  }
   user: User = new User();
 
   openDialog() {
